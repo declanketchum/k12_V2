afac4e1df1be99f01b1d86b79a6f90aa2e61d20f
declanketchum <dketchum@olin.edu>
2024-02-09 16:19:20 -0500

    restarting using gasby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,36 +3,22 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `K-12 Workshops`,
+    title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }, 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "content",
-      "path": "./src/content/"
-    },
-    __key: "content"
-  },
-  {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  },
- ]
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
 };
